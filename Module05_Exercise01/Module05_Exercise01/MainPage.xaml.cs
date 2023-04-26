0d4d3529250af5b3a6860ead1a8b0f76cad593bf
jeremiahquiambao <quiambao.jeremiah@auf.edu.ph>
2023-04-26 20:56:41 +0800

    first commit

---
+++ Module05_Exercise01/Module05_Exercise01/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module05_Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnSubmitButtonClicked(object sender, EventArgs e)
+        {
+            string name = nameEntry.Text;
+            DateTime birthDate = datePicker.Date;
+            bool isStudent = studentSwitch.IsToggled;
+            string studentStatus = isStudent ? "Yes" : "No";
+            DisplayAlert("Hello!", $"Hello {name}! You were born on {birthDate.ToString("d")}. Are you a student? {studentStatus}", "OK");
+        }
+    }
+}
